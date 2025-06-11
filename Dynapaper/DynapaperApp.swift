8c156b1f613d4ac6f81912be984dcb92e690343f
Acrylic M <akirill1942@me.com>
2025-06-11 12:55:06 +0300

    Initial Commit

---
+++ Dynapaper/DynapaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynapaperApp.swift
+//  Dynapaper
+//
+//  Created by Acrylic M on 11.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct DynapaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DynapaperTests/DynapaperTests.swift
@@ -0,0 +1,17 @@
+//
+//  DynapaperTests.swift
+//  DynapaperTests
+//
+//  Created by Acrylic M on 11.06.2025.
+//
+
+import Testing
+@testable import Dynapaper
+
+struct DynapaperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
